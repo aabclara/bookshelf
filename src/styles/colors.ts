f176c4c6bca0b884ff0d0f9e3ad4f3a0eb629578
“clarawm” <mariaclaramiguel.c@gmail.com>
2023-03-06 12:23:17 -0300

    “Página5”

---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#FFB890',
+    secondary: '#887FF6',
+    third: '##D9D9D9',
+    thirdLight: 'rgba(217,217,217,0.8)',
+    black: '#000',
+    white: '#FFF'
+}
